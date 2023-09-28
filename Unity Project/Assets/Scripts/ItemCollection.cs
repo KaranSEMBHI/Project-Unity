eb7ccc6d6a8afc8e579d02fe37e0082335fa2497
KaranSEMBHI <127668278+KaranSEMBHI@users.noreply.github.com>
2023-09-28 11:57:25 +0200

    Itemcollection fixed + animations added

--- Unity Project/Assets/Scripts/ItemCollection.cs
+++ Unity Project/Assets/Scripts/ItemCollection.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class ItemCollection : MonoBehaviour
 {
+    private int bananas = 0;    
     // Deze methode wordt automatisch aangeroepen wanneer het GameObject waarop dit script is bevestigd
     // een trigger-collider raakt van een ander GameObject.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Controleer of het andere GameObject dat de trigger raakte is getagd als "Apple".
-        if (collision.gameObject.CompareTag("Apple"))
+        if (collision.gameObject.CompareTag("Banana"))
         {
-            // Als het raakte object is getagd als "Apple", vernietig het huidige GameObject.
+            // Als het raakte object is getagd als "Banana", vernietig het huidige GameObject.
             // Het huidige GameObject is het object waarop dit script is bevestigd.
-            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            bananas++;
+            Debug.Log("Bananas: " + bananas);
+
+
         }
     }
 }
--- Unity Project/Assets/Scripts/PlayerMovement.cs
+++ Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,21 @@ public class PlayerMovement : MonoBehaviour
 
     // Referentie naar de Rigidbody2D-component van de speler
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;  
 
     // Variabele om te controleren of de speler de grond raakt
     private bool raaktDeGrond;
+
+    // Variabele voor de horizontale input van de speler
+    private float horizontaleInput = 0f;
 
     void Start()
     {
         // Haal de Rigidbody2D-component op bij de speler
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -24,13 +31,33 @@ public class PlayerMovement : MonoBehaviour
         raaktDeGrond = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
 
         // Horizontale beweging
-        float horizontaleInput = Input.GetAxis("Horizontal");
+        horizontaleInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontaleInput * snelheid, rb.velocity.y);
 
         // Springen als de spatiebalk wordt ingedrukt en de speler op de grond staat
         if (raaktDeGrond && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * sprongKracht, ForceMode2D.Impulse);
+        }
+
+        UpdateAnimationState();
+    }
+    private void UpdateAnimationState()
+    {
+
+        if (horizontaleInput > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (horizontaleInput < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
