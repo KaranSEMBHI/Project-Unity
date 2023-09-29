f61bf319451e0815743355e5488b99b9f2eaf054
KaranSEMBHI <127668278+KaranSEMBHI@users.noreply.github.com>
2023-09-29 21:36:45 +0200

    Commit 4

--- Unity Project/Assets/Scripts/PlayerMovement.cs
+++ Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,23 @@ public class PlayerMovement : MonoBehaviour
     // Referentie naar de Rigidbody2D-component van de speler
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
-    private Animator anim;  
+    private Animator anim;
+
+    private enum MovementState
+    {
+        idle,
+        running,
+        jumping,
+        falling
+    }
 
     // Variabele om te controleren of de speler de grond raakt
     private bool raaktDeGrond;
 
     // Variabele voor de horizontale input van de speler
     private float horizontaleInput = 0f;
+
+    private bool hasJumped = false; // Add this variable to track if the player has jumped
 
     void Start()
     {
@@ -34,30 +44,47 @@ public class PlayerMovement : MonoBehaviour
         horizontaleInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontaleInput * snelheid, rb.velocity.y);
 
-        // Springen als de spatiebalk wordt ingedrukt en de speler op de grond staat
-        if (raaktDeGrond && Input.GetKeyDown(KeyCode.Space))
+        // Springen als de spatiebalk wordt ingedrukt, de speler op de grond staat, en nog niet heeft gesprongen
+        if (raaktDeGrond)
+        {
+            hasJumped = false; // Reset the flag to false when touching the ground
+        }
+
+        if (raaktDeGrond && Input.GetKeyDown(KeyCode.Space) && !hasJumped)
         {
             rb.AddForce(Vector2.up * sprongKracht, ForceMode2D.Impulse);
+            hasJumped = true; // Set the flag to true after jumping
         }
 
         UpdateAnimationState();
     }
+
     private void UpdateAnimationState()
     {
-
+        MovementState state;
         if (horizontaleInput > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (horizontaleInput < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("state", (int)state);
     }
 }
